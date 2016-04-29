4ef0b7edaa2ee365a4f66b6d33db225193b26b19
JBlond <leet31337@web.de>
2016-04-29 17:14:55 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp ./.bash_logout ../
+cp ./.bashrc ../
+cp ./.config ../
+cp ./.gitconfig ../
+cp ./.git-prompt.sh ../
+cp ./.nanorc ../
+cp ./.profile ../
+cp ./.vimrc ../
+source ~/.bashrc

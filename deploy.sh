21e9542a64bd5f6d2d7ea27de3fb23f7febfa982
JBlond <leet31337@web.de>
2016-07-29 10:36:02 +0200

    create symlinks

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cp ./.bash_logout ../
-cp ./.bashrc ../
 cp -r ./.config ../
-cp ./.gitconfig ../
-cp ./.git-prompt.sh ../
-cp ./.nanorc ../
-cp ./.profile ../
-cp ./.vimrc ../
+ln $HOME/dotfiles/.bash_logout $HOME/.bash_logout
+ln $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln $HOME/dotfiles/.git-prompt.sh $HOME/.git-prompt.sh
+ln $HOME/dotfiles/.minttyrc $HOME/.minttyrc
+ln $HOME/dotfiles/.nanorc $HOME/.nanorc
+ln $HOME/dotfiles/.profile $HOME/.profile
+ln $HOME/dotfiles/.vimrc $HOME/.vimrc
 source ~/.bashrc

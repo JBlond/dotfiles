7f1ad824e6f22242b976ef1cfaae75b02c5253c0
JBlond <leet31337@web.de>
2016-08-29 15:10:16 +0200

    kill before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+rm -rf $HOME/.config
+rm -f $HOME/.bash_logout
+rm -f $HOME/.bashrc
+rm -f $HOME/.gitconfig
+rm -f $HOME/.gitconfig
+rm -f $HOME/.git-prompt.sh
+rm -f $HOME/.minttyrc
+rm -f $HOME/.nanorc
+rm -f $HOME/.profile
+rm -rf $HOME/.vimrc
 cp -r ./.config ../
 ln $HOME/dotfiles/.bash_logout $HOME/.bash_logout
 ln $HOME/dotfiles/.bashrc $HOME/.bashrc

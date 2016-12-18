c0126e1df2414df2284839ef709656cbe6bf803c
JBlond <leet31337@web.de>
2016-12-18 21:57:28 +0100

    apply tos

--- update_cert.sh
+++ update_cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo /opt/apache2/bin/httpd -k stop
-sudo /root/certbot/letsencrypt-auto renew
+sudo /root/certbot/letsencrypt-auto renew --agree-tos
 sudo /opt/apache2/bin/httpd -k start

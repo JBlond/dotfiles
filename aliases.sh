a50d63254d486b7f9d5ea12a3ddb82cb3cf67550
JBlond <leet31337@web.de>
2017-07-21 11:26:00 +0200

    move things to dotfiles

---
+++ aliases.sh
@@ -0,0 +1,28 @@
+# Alias definitions.
+eval "`dircolors -b`"
+alias ls='ls --color=auto --group-directories-first'
+alias dir='ls --color=auto --format=vertical'
+
+alias grep='grep --color=auto --exclude-dir="node_modules"'
+alias fgrep='fgrep --color=auto --exclude-dir="node_modules"'
+alias egrep='egrep --color=auto --exclude-dir="node_modules"'
+
+#debian
+#alias upgrade='sudo aptitude update && sudo aptitude dist-upgrade'
+#updates are fetched via cron-apt
+alias upgrade='sudo aptitude dist-upgrade'
+
+# some more ls aliases
+alias ll='ls -lh'
+alias la='ls -A'
+alias l='ls -CF'
+alias us="ls -la | grep ^- | awk '{print \$9}' | grep ^\\\."
+alias nano='nano -wc'
+alias ..="cd .."
+alias cd..="cd .."
+alias more='less'
+alias ping5='ping -c 5'
+alias dush='du . -sh'
+
+alias lll="stat --format='%a %U %G %s %y %N' *"
+
--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@ cp -r ./.config ../
 ln $HOME/dotfiles/.bash_logout $HOME/.bash_logout
 ln $HOME/dotfiles/.bashrc $HOME/.bashrc
 ln $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-ln $HOME/dotfiles/.git-prompt.sh $HOME/.git-prompt.sh
 ln $HOME/dotfiles/.minttyrc $HOME/.minttyrc
 ln $HOME/dotfiles/.nanorc $HOME/.nanorc
 ln $HOME/dotfiles/.profile $HOME/.profile

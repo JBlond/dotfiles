79c1caebf8f31552f8e1d4fadbe400f51d8616e3
JBlond <leet31337@web.de>
2017-07-22 21:37:31 +0200

    red x

--- .git_functions.sh
+++ .git_functions.sh
@@ -1,11 +1,11 @@
 find_git_dirty () {
-  if [[ ! -z $(__git_ps1) && -n $(git status --porcelain) ]]; then echo "✗"; fi
+  if [[ ! -z $(__git_ps1) && -n $(git status --porcelain) ]]; then printf "\e[41m✗"; fi
 }
 
 find_git_commit_diff () {
   if [[ ! -z $(__git_ps1) ]]; then
     commit_diff=$(git for-each-ref --format="%(push:track)" refs/heads)
-    commit_diff=${commit_diff//ahead\ /\+}
+    commit_diff=${commit_diff//ahead\ /\☆}
     commit_diff=${commit_diff//behind\ /\-}
     echo $commit_diff
   fi

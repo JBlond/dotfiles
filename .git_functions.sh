858afede249428c8c3a865e80b8103b8f53540f0
JBlond <leet31337@web.de>
2017-07-23 18:28:46 +0200

    fix clean status

--- .git_functions.sh
+++ .git_functions.sh
@@ -2,29 +2,37 @@ find_git_dirty () {
 	if [[ -z $(__git_ps1) ]]; then
 		exit
 	fi
+	local clean="clean"
 	gitstatus=$(git status --porcelain | sed s/^.// | cut -d' ' -f1)
 	for line in $gitstatus; do
 		if [ "$line" = "D" ]; then
 			printf " ✗"
+			clean="dirty"
 		elif [ "$line" = "M" ]; then
 			printf " \e[41m✗\033[0m";
+			clean="dirty"
 		fi
 	done
-	if [ -z "$gitstatus" ]; then
-		printf " \e[32m✓"
-	fi
+
 	modifiedfiles=$(git status --porcelain | grep "^M" | cut -c 4-)
 	for line1 in $modifiedfiles; do
 		printf " \e[41m✗";
+		clean="dirty"
 	done
 	addedfiles=$(git status --porcelain | grep "^A" | cut -c 4-)
 	for line2 in $addedfiles; do
 		printf " Ξ"
+		clean="dirty"
 	done
 	untracked=$(git ls-files --others --exclude-standard)
 	for line3 in $untracked; do
 		printf " 🙈"
+		clean="dirty";
 	done
+
+	if [[ -z "$gitstatus" && "$clean" == "clean" ]]; then
+		printf " \e[32m✓"
+	fi
 }
 
 find_git_commit_diff () {

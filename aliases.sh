66865d79bc891e3ab153808566b4ce2af3c7b663
mbrandt <mbrandt@isa-hamburg.com>
2017-10-17 10:34:22 +0200

    composer in Docker

--- aliases.sh
+++ aliases.sh
@@ -28,3 +28,16 @@ alias lll="stat --format='%a %U %G %s %y %N' *"
 
 alias :D="echo ツ"
 
+dcomposer () {
+    tty=
+    tty -s && tty=--tty
+    docker run \
+        $tty \
+        --interactive \
+        --rm \
+        --user $(id -u):$(id -g) \
+        --volume /etc/passwd:/etc/passwd:ro \
+        --volume /etc/group:/etc/group:ro \
+        --volume $(pwd):/app \
+        composer "$@"
+}

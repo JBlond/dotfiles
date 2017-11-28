ad82ff7ce80e06d6411202ad02962d9ae419e68d
Mario <JBlond@users.noreply.github.com>
2017-11-28 14:58:33 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,15 @@ eval "`dircolors -b`"
 alias ls='ls --color=auto --group-directories-first'
 alias dir='ls --color=auto --format=vertical'
 
+alias ll='ls -lh'
+alias la='ls -A'
+alias l='ls -CF'
+alias us="ls -la | grep ^- | awk '{print \$9}' | grep ^\\\."
+alias lll="stat --format='%a %U %G %s %y %N' *"
+
+alias ..='cd ..'
+alias cd..='cd ..'
+
 alias grep='grep --color=auto --exclude-dir="node_modules"'
 alias fgrep='fgrep --color=auto --exclude-dir="node_modules"'
 alias egrep='egrep --color=auto --exclude-dir="node_modules"'
@@ -10,22 +19,16 @@ alias egrep='egrep --color=auto --exclude-dir="node_modules"'
 #debian
 #alias upgrade='sudo aptitude update && sudo aptitude dist-upgrade'
 #updates are fetched via cron-apt
-alias upgrade='sudo apt dist-upgrade'
 alias update='sudo apt update'
+alias upgrade='sudo apt dist-upgrade'
 
-# some more ls aliases
-alias ll='ls -lh'
-alias la='ls -A'
-alias l='ls -CF'
-alias us="ls -la | grep ^- | awk '{print \$9}' | grep ^\\\."
 alias nano='nano -wc'
-alias ..='cd ..'
-alias cd..='cd ..'
 alias more='less'
-alias ping5='ping -c 5'
-alias dush='du . -sh'
 
-alias lll="stat --format='%a %U %G %s %y %N' *"
+alias ping5='ping -c 5'
+alias flushdns="sudo /etc/init.d/dns-clean restart && echo DNS cache flushed"
+
+alias dush='du . -sh'
 
 alias :D="echo ツ"
 alias dontcare='echo ¯\_\(ツ\)_/¯'

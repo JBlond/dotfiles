2ebcff7e90aac56134bf7bd721f4a2dd832d5c61
Mario <JBlond@users.noreply.github.com>
2018-01-23 16:45:34 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -33,3 +33,8 @@ matrix () {
         sleep 0.05; 
     done|awk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
 }
+
+# check processes using a port
+function port() {
+	lsof -iTCP:$1 -sTCP:LISTEN
+}

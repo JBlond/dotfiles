949a461197d4802f03a15362ef60ca3a7178b792
JBlond <leet31337@web.de>
2018-02-15 23:18:41 +0100

    remove xterm

--- xterm.sh
+++
@@ -1,8 +0,0 @@
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
-    ;;
-*)
-    ;;
-esac

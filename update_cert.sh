7cf4f21de0e768b709dcb9d94e6ebeaa48e69e82
Mario <JBlond@users.noreply.github.com>
2018-03-16 12:45:31 +0100

    Update update_cert.sh

--- update_cert.sh
+++ update_cert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# repo  https://github.com/certbot/certbot
 sudo /opt/apache2/bin/httpd -k stop
 sudo /root/certbot/letsencrypt-auto renew --agree-tos --no-self-upgrade --no-bootstrap
 sudo /opt/apache2/bin/httpd -k start

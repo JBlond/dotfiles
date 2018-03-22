f8f0b5f4f0eb6bca65afe23d5d0c68b5ad088909
JBlond <leet31337@web.de>
2018-03-22 13:41:24 +0100

    rename

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ rm -rf $HOME/.vimrc
 rm -rf $HOME/.vim
 rm -f $HOME/.tmux.conf
 cp -r ./.config ../
-ln $HOME/dotfiles/home/.bash_logout $HOME/.bash_logout
-ln $HOME/dotfiles/home/.bashrc $HOME/.bashrc
+ln $HOME/dotfiles/home/bash_logout $HOME/.bash_logout
+ln $HOME/dotfiles/home/bashrc $HOME/.bashrc
 ln $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
-ln $HOME/dotfiles/home/.minttyrc $HOME/.minttyrc
-ln $HOME/dotfiles/home/.nanorc $HOME/.nanorc
-ln $HOME/dotfiles/home/.profile $HOME/.profile
+ln $HOME/dotfiles/home/minttyrc $HOME/.minttyrc
+ln $HOME/dotfiles/home/nanorc $HOME/.nanorc
+ln $HOME/dotfiles/home/profile $HOME/.profile
 ln -s $HOME/dotfiles/vim $HOME/.vim
-ln $HOME/dotfiles/home/.vimrc $HOME/.vimrc
-ln $HOME/dotfiles/home/.tmux.conf $HOME/.tmux.conf
+ln $HOME/dotfiles/home/vimrc $HOME/.vimrc
+ln $HOME/dotfiles/home/tmux.conf $HOME/.tmux.conf
 source ~/.bashrc

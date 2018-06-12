db8d65ca820804c4ee13321f0f409ebaea3fac84
JBlond <leet31337@web.de>
2018-06-12 17:05:09 +0200

    complete with arrow keys

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ rm -f $HOME/.bashrc
 rm -f $HOME/.gitconfig
 rm -f $HOME/.nanorc
 rm -f $HOME/.profile
+rm -f $HOME/.inputrc
 rm -rf $HOME/.vim
 if [[ "$OSTYPE" != "msys" ]]; then
 	rm -f $HOME/.tmux.conf
@@ -15,6 +16,7 @@ ln $HOME/dotfiles/home/bashrc $HOME/.bashrc
 ln $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
 ln $HOME/dotfiles/home/nanorc $HOME/.nanorc
 ln $HOME/dotfiles/home/profile $HOME/.profile
+ln $HOME/dotfiles/home/inputrc $HOME/.inputrc
 ln -s $HOME/dotfiles/vim $HOME/.vim
 if [[ "$OSTYPE" == "msys" ]]; then
 	rm -f $HOME/.minttyrc

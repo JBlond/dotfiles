1fe4c9a83d955beb1c8901b9ca8f09d84b663a47
JBlond <leet31337@web.de>
2018-06-25 16:04:05 +0200

    update

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -44,6 +44,8 @@ alias ping5='ping -c 5'
 alias flushdns="sudo /etc/init.d/dns-clean restart && echo DNS cache flushed"
 alias pubip="dig +short myip.opendns.com @resolver1.opendns.com"
 
+alias mssh="mosh"
+
 alias wttr='curl -L wttr.in/?lang=de'
 alias :D="echo ツ"
 alias shangdi="echo 上帝就是愛"

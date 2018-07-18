c4d382d89085b9bbc4aa3258627c7fd049e11235
JBlond <leet31337@web.de>
2018-07-18 12:25:12 +0200

    add lock for windows

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -65,6 +65,7 @@ alias :q="exit"
 if [[ "$OSTYPE" == "msys" ]]; then
 	alias apache='~/Apache24/bin/httpd.exe'
 	alias ifconfig="ipconfig -all"
+	alias lock="rundll32.exe user32.dll,LockWorkStation"
 else
 	alias apache='sudo /opt/apache2/bin/httpd'
 fi

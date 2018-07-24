f6ef030b111a51ba011c6c8f8a5a3baaae5bfb80
JBlond <leet31337@web.de>
2018-07-24 13:05:42 +0200

    add hibernate

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -64,6 +64,7 @@ alias :q="exit"
 
 if [[ "$OSTYPE" == "msys" ]]; then
 	alias apache='~/Apache24/bin/httpd.exe'
+	alias hibernate="rundll32.exe powrprof.dll,SetSuspendState"
 	alias ifconfig="ipconfig -all"
 	alias lock="rundll32.exe user32.dll,LockWorkStation"
 else

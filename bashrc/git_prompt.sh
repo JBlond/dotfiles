34d601dc5ab606befaa467331fc11d9d5f284bc9
JBlond <leet31337@web.de>
2018-08-27 18:39:09 +0200

    add branch symbol

--- bashrc/git_prompt.sh
+++ bashrc/git_prompt.sh
@@ -376,7 +376,7 @@ __git_ps1 ()
 		if [[ $gitstring = "master" ]] || [[ $gitstring = "BARE:master"  ]]; then
 			printf -- "$printf_format" "♆ $gitstring"
 		else
-			#branch name with ♆
+			#branch name with ♆ 🌿
 			printf -- "$printf_format" "♆ $gitstring"
 		fi
 	fi

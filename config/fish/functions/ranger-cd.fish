2f3e31b9ff93319cd87a22a0efd32f50135528eb
JBlond <leet31337@web.de>
2019-08-06 18:43:25 +0200

    fix ranger for git on windows

--- config/fish/functions/ranger-cd.fish
+++ config/fish/functions/ranger-cd.fish
@@ -1,4 +1,7 @@
 function ranger-cd
+	if not test -f /tmp/pwd-from-ranger
+		touch /tmp/pwd-from-ranger
+	end	
 	set tmpfile "/tmp/pwd-from-ranger"
 	ranger --choosedir=$tmpfile $argv
 	set rangerpwd (cat $tmpfile)

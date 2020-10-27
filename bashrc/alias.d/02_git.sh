4f57d1868a1b46e62f0a24ff8befc612c31799d0
JBlond <leet31337@web.de>
2020-10-27 14:31:32 +0100

    remove unused aliases

--- bashrc/alias.d/02_git.sh
+++ bashrc/alias.d/02_git.sh
@@ -3,27 +3,12 @@
 source ~/dotfiles/git/complete
 complete -o default -o nospace -F _git git
 
-alias ga="git add"
-alias gbr="git branch"
-alias gcfg="git config --list"
 alias gcl="git clone --recurse-submodules"
 alias gcma="git cma"
 alias gcmap="git cmap"
 alias gco="git checkout"
 alias gd="git diff"
-alias gdiff="git diff"
-alias gg="git branch -vv"
 alias ggc="git gc"
 alias gh='cd "$(git rev-parse --show-toplevel)"'
-alias gi="git init"
-alias gib="git init --bare"
-alias gitu="git cmapu"
-alias gl="git lg"
-alias glc="git diff @~..@"
-alias gm="git checkout master"
-alias gp="git pull --progress --no-rebase"
-alias gpn="git pull --no-ff"
-alias gpo="git push origin"
-alias gr="git remote -v"
 alias gst="git status -sb"
 alias gsu="git submodule update --recursive --remote"

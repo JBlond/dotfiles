047777e0898883e2ab0f12a621717924b6593e54
Mario <JBlond@users.noreply.github.com>
2021-02-10 14:55:41 +0100

    Create add_path_maybe.fish

---
+++ config/fish/functions/add_path_maybe.fish
@@ -0,0 +1,12 @@
+# Add a directory to the path, but only if it exists.
+
+function add_path_maybe -d "Add a directory to the path, but only if it exists"
+    # If the path exists...
+    if test -d $argv[ 1 ]
+        # ...and if it's not already in the PATH...
+        if not contains $ARGV[ 1 ] $PATH
+            # ...push it to the start of the path.
+            set PATH $argv[ 1 ] $PATH
+        end
+    end
+end

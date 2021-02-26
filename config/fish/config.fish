139c6a49de41309444a43b6b56dcaa754916730f
JBlond <leet31337@web.de>
2021-02-26 15:18:31 +0100

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -86,7 +86,7 @@ abbr sc 'systemctl'
 alias grep='grep --color=auto --exclude-dir="node_modules"'
 alias fgrep='fgrep --color=auto --exclude-dir="node_modules"'
 alias egrep='egrep --color=auto --exclude-dir="node_modules"'
-alias rns='grep -Rins $1 *'
+alias rns='grep -Rins $argv *'
 
 abbr download "curl -LO "
 

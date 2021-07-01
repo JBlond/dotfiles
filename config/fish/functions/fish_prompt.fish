3d2b43a8120ecee2bb3b82d006bf4444f56996ab
JBlond <leet31337@web.de>
2021-07-01 12:41:04 +0200

    Fix: fatal: not a git repository (or any of the parent directories): .git

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  echo (command git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _is_git_dirty

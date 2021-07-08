c9f42d4d96ccd9c33075874c292788422d0295a6
JBlond <leet31337@web.de>
2021-07-08 12:35:20 +0200

    Add os based kill

---
+++ config/fish/functions/mkill.fish
@@ -0,0 +1,9 @@
+function mkill --description 'Always kill the process by PID'
+   switch (uname -o)
+        case Msys
+            taskkill /F /PID $1
+        case "*"
+            kill -9 $1
+    end
+    echo "( ︶︿︶)_╭∩╮"
+end

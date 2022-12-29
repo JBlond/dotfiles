95e137b99139fcb16bcd7341dd00b2609b672669
JBlond <leet31337@web.de>
2022-12-29 23:36:16 +0100

    Update emoji fonts

--- fonts.sh
+++ fonts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /usr/share/mintty
-curl -LO https://github.com/JBlond/emojis/archive/refs/tags/1.0.0.zip
-unzip 1.0.0.zip
-mv emojis-1.0.0 emojis
-rm 1.0.0.zip
-
+curl -LO https://github.com/JBlond/emojis/archive/refs/tags/1.1.0.zip
+unzip 1.1.0.zip
+mv emojis-1.1.0 emojis
+rm 1.1.0.zip

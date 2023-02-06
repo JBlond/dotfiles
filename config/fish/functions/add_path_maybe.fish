5762af46ab70922756a589551b1682431c69c2da
JBlond <leet31337@web.de>
2023-02-06 09:21:31 +0100

    use native fish function to add to PATH

--- config/fish/functions/add_path_maybe.fish
+++ config/fish/functions/add_path_maybe.fish
@@ -6,7 +6,7 @@ function add_path_maybe -d "Add a directory to the path, but only if it exists"
         # ...and if it's not already in the PATH...
         if not contains $argv[1] $PATH
             # ...push it to the start of the path.
-            set PATH $argv[1] $PATH
+            fish_add_path $argv[1]
         end
     end
 end

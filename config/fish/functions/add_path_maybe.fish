25deb54a90734f9fba3d0e90fdf5fa00d3f28ade
JBlond <leet31337@web.de>
2023-02-07 09:20:54 +0100

    Revert that

--- config/fish/functions/add_path_maybe.fish
+++ config/fish/functions/add_path_maybe.fish
@@ -6,7 +6,7 @@ function add_path_maybe -d "Add a directory to the path, but only if it exists"
         # ...and if it's not already in the PATH...
         if not contains $argv[1] $PATH
             # ...push it to the start of the path.
-            fish_add_path $argv[1]
+            set PATH $argv[1] $PATH
         end
     end
 end

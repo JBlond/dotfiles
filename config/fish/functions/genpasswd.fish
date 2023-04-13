8c4d5928a71de8212f900c5bb138e720f3b562af
JBlond <leet31337@web.de>
2023-04-13 13:41:35 +0200

    update

--- config/fish/functions/genpasswd.fish
+++
@@ -1,4 +0,0 @@
-function genpasswd --wraps=echo\ `env\ LC_CTYPE=C\ tr\ -dc\ \"a-zA-Z0-9-_\\\$\\\?\"\ \<\ /dev/urandom\ \|\ head\ -c\ 20` --description alias\ genpasswd=echo\ `env\ LC_CTYPE=C\ tr\ -dc\ \"a-zA-Z0-9-_\\\$\\\?\"\ \<\ /dev/urandom\ \|\ head\ -c\ 20`
-  echo `env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 20` $argv
-        
-end

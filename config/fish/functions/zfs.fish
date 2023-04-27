45627f6e15d5d344acfffa27102d146911dae879
JBlond <leet31337@web.de>
2023-04-27 11:53:17 +0200

    set new zfs shortcut

--- config/fish/functions/zfs.fish
+++ config/fish/functions/zfs.fish
@@ -2,6 +2,8 @@ function zfs
     switch $argv[1]
         case destory
             set argv[1] destroy
+        case ls
+             set argv[1] list -t snapshot
     end
     command zfs $argv
 end

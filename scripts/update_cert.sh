f313f0e51467bcdcf4f1569436e7ef2e2754e8d5
JBlond <leet31337@web.de>
2023-05-10 10:54:11 +0200

    Update cert script

--- scripts/update_cert.sh
+++ scripts/update_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # repo  https://github.com/certbot/certbot
 sudo /opt/apache2/bin/httpd -k stop
-sudo certbot renew --agree-tos --no-self-upgrade --no-bootstrap
+sudo certbot renew --agree-tos
 sudo /opt/apache2/bin/httpd -k start

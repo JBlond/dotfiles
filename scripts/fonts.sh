bb104f022c7b13a7e7b18e945679063f3ed99042
JBlond <leet31337@web.de>
2023-10-20 08:57:07 +0200

    Update emojis

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /usr/share/mintty
-curl -LO https://github.com/JBlond/emojis/archive/refs/tags/1.1.0.zip
-unzip 1.1.0.zip
-mv emojis-1.1.0 emojis
-rm 1.1.0.zip
+curl -LO https://github.com/JBlond/emojis/archive/refs/tags/1.2.0.zip
+unzip 1.2.0.zip
+mv emojis-1.2.0 emojis
+rm 1.2.0.zip

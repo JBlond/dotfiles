a62ba6f34d88e7db9f69877307825a9fe66e11db
JBlond <leet31337@web.de>
2024-01-04 19:14:30 +0100

    update

--- bin/crypt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ "$1" == "de" || "$1" == "dec" || "$1" == "decrypt" ]]; then
-	openssl enc -d -aes-256-ctr -pbkdf2 -in $2 -out $3
-fi
-
-if [[ "$1" == "en" || "$1" == "enc" || "$1" == "encrypt" ]]; then
-    openssl enc -e -aes-256-ctr -pbkdf2 -in $2 -out $2.enc
-	#_encrypt
-fi

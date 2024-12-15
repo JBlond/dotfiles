cb06fe1197029d20d0174ecc9c062aea483c0421
JBlond <leet31337@web.de>
2024-12-15 19:54:54 +0100

    Move aliases into its own file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,38 +11,6 @@ add_path_maybe $HOME/notes/bin $PATH
 add_path_maybe /usr/local/go/bin
 add_path_maybe /opt/nvim-linux64/bin
 
-if test -d $HOME/ranger
-	alias ranger="$HOME/ranger/ranger.py"
-end
-
-alias cursor='echo -ne "\e[3 q"'
-
-# Enable aliases to be sudo’ed
-alias sudo='sudo '
-
-alias hibernate="rundll32.exe powrprof.dll,SetSuspendState"
-
-alias journalctl='sudo journalctl'
-alias systemctl='sudo systemctl'
-
-alias grep='grep --color=auto --exclude-dir="node_modules"'
-alias fgrep='fgrep --color=auto --exclude-dir="node_modules"'
-alias egrep='egrep --color=auto --exclude-dir="node_modules"'
-alias rns='grep -Rns $argv *'
-
-alias shit="echo 💩"
-alias :D="echo ツ"
-alias dontcare='echo ¯\\\_\(ツ\)_/¯'
-alias busy='cat /dev/urandom | hexdump -C | grep "ca fe"'
-alias shangdi="echo 上帝就是愛"
-
-alias nano='nano -wc'
-
-alias vless='/usr/share/vim/vim8*/macros/less.sh'
-alias :e="vim"
-# use vim alias to exit shell
-alias :q="exit"
-
 # To bind Ctrl-O to ranger-cd, save this in `~/.config/fish/config.fish`:
 bind \co ranger-cd
 

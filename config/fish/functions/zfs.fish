34f80cd170a63d1cb13613457292aa72b8dfe143
JBlond <leet31337@web.de>
2025-09-03 13:55:03 +0200

    Abbort if not available

--- config/fish/functions/zfs.fish
+++ config/fish/functions/zfs.fish
@@ -1,13 +1,15 @@
 function zfs
-	if type -q zfs
-		switch $argv[1]
-			case destory
-				set argv[1] destroy
-			case ls
-				set argv[1] list
-				set argv[2] -t
-				set argv[3] snapshot
-		end
-		command zfs $argv
+	if not type -q zfs
+		echo "Fehler: ZFS ist auf diesem System nicht installiert." >&2
+		return 1
 	end
+	switch $argv[1]
+		case destory
+			set argv[1] destroy
+		case ls
+			set argv[1] list
+			set argv[2] -t
+			set argv[3] snapshot
+	end
+	command zfs $argv
 end

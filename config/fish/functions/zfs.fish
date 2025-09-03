f55ecd8a3c2f616fea0199396b8fc6b2b9110e8b
JBlond <leet31337@web.de>
2025-09-03 12:55:04 +0200

    update

--- config/fish/functions/zfs.fish
+++ config/fish/functions/zfs.fish
@@ -1,11 +1,13 @@
 function zfs
-	switch $argv[1]
-		case destory
-			set argv[1] destroy
-		case ls
-			set argv[1] list
-			set argv[2] -t
-			set argv[3] snapshot
+	if type -q zfs
+		switch $argv[1]
+			case destory
+				set argv[1] destroy
+			case ls
+				set argv[1] list
+				set argv[2] -t
+				set argv[3] snapshot
+		end
+		command zfs $argv
 	end
-	command zfs $argv
 end

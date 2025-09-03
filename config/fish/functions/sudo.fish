c1d51246262f7b1617666a3d70cc8ceba4bdc557
JBlond <leet31337@web.de>
2025-09-03 15:52:05 +0200

    Abbort if sudo is not available

--- config/fish/functions/sudo.fish
+++ config/fish/functions/sudo.fish
@@ -1,4 +1,9 @@
 function sudo --description 'alias sudo=sudo '
+	set real_sudo (command -v sudo)
+	if test -z "$real_sudo"
+		echo "Error: sudo is not installed on this system" >&2
+		return 1
+	end
 	# Enable aliases to be sudo’ed
 	command sudo  $argv
 end

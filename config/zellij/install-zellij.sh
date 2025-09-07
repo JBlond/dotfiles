2112ff78717c186700aaa1a7ddd669f5ff2d350a
JBlond <leet31337@web.de>
2025-09-07 16:29:58 +0200

    update

--- config/zellij/install-zellij.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo rm -rf /usr/local/bin/zellij
-curl -LO https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
-sudo tar -C /usr/local/bin -xzf zellij-x86_64-unknown-linux-musl.tar.gz
-rm zellij-x86_64-unknown-linux-musl.tar.gz

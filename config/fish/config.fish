73131221b8bfa509a6c9a36463cbb13a3b7efdd8
JBlond <leet31337@web.de>
2025-12-07 20:41:57 +0100

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -56,30 +56,26 @@ if test -f /usr/share/cachyos-fish-config/config.fish
 	source /usr/share/cachyos-fish-config/config.fish
 end
 
-set -Ux fish_color_cancel "\x2dr"
-set -Ux fish_color_command "005fd7"
-set -Ux fish_color_comment "990000"
-set -Ux fish_color_cwd "green"
-set -Ux fish_color_cwd_root "red"
-set -Ux fish_color_end "009900"
-set -Ux fish_color_error "ff0000"
-set -Ux fish_color_escape "00a6b2"
-set -Ux fish_color_history_current "\x2d\x2dbold"
-set -Ux fish_color_host "normal"
-set -Ux fish_color_match "\x2d\x2dbackground\x3dbrblue"
-set -Ux fish_color_normal "normal"
-set -Ux fish_color_operator "00a6b2"
-set -Ux fish_color_param "00afff"
-set -Ux fish_color_quote "999900"
-set -Ux fish_color_redirection "00afff"
-set -Ux fish_color_search_match "bryellow\x1e\x2d\x2dbackground\x3dbrblack"
-set -Ux fish_color_selection "white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack"
-set -Ux fish_color_user "brgreen"
-set -Ux fish_color_valid_path "\x2d\x2dunderline"
-set -Ux fish_greeting "Welcome\x20to\x20fish\x2c\x20the\x20friendly\x20interactive\x20shell"
-set -Ux fish_key_bindings "fish_default_key_bindings"
-set -Ux fish_pager_color_description "B3A06D\x1eyellow"
-set -Ux fish_pager_color_prefix "white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline"
-set -Ux fish_pager_color_progress "brwhite\x1e\x2d\x2dbackground\x3dcyan"
-set -Ux fish_prompt_pwd_dir_length 0
+set -U fish_color_autosuggestion 555 --background=brblack
+set -U fish_color_cancel -r
+set -U fish_color_command 005fd7
+set -U fish_color_comment 990000
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end 009900
+set -U fish_color_error ff0000
+set -U fish_color_escape 00a6b2
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator 00a6b2
+set -U fish_color_param 00afff
+set -U fish_color_quote 999900
+set -U fish_color_redirection 00afff
 
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_selection white --bold --background=brblack
+
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline

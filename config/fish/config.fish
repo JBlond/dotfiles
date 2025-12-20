ee1cae18c9f357e1dbaf034345311fbeb6fdfe62
JBlond <leet31337@web.de>
2025-12-20 19:48:27 +0100

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -57,26 +57,4 @@ if test -f /usr/share/cachyos-fish-config/config.fish
 	source /usr/share/cachyos-fish-config/config.fish
 end
 
-set -U fish_color_autosuggestion 555 --background=brblack
-set -U fish_color_cancel -r
-set -U fish_color_command 005fd7
-set -U fish_color_comment 990000
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_end 009900
-set -U fish_color_error ff0000
-set -U fish_color_escape 00a6b2
-set -U fish_color_history_current --bold
-set -U fish_color_host normal
-set -U fish_color_match --background=brblue
-set -U fish_color_normal normal
-set -U fish_color_operator 00a6b2
-set -U fish_color_param 00afff
-set -U fish_color_quote 999900
-set -U fish_color_redirection 00afff
-
-set -U fish_color_search_match bryellow --background=brblack
-set -U fish_color_selection white --bold --background=brblack
-
-set -U fish_color_user brgreen
-set -U fish_color_valid_path --underline
+fish_colors

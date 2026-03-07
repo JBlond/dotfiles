e375ea87e447dd0b2546749d4d5379595e588331
JBlond <leet31337@web.de>
2026-03-07 10:25:20 +0100

    update

--- config/fish/conf.d/alias.fish
+++ config/fish/conf.d/alias.fish
@@ -11,3 +11,17 @@ end
 alias :q="exit"
 
 alias vless "/usr/share/vim/vim9*/macros/less.sh"
+
+alias ansible 'docker run -ti --rm \
+  -v ~/.ssh:/mnt/ssh \
+  -v (pwd):/apps \
+  -w /apps \
+  --network host \
+  alpine/ansible sh -c "mkdir -p /root/.ssh && cp -r /mnt/ssh/* /root/.ssh/ && chmod 700 /root/.ssh && chmod 600 /root/.ssh/* && exec ansible $argv"'
+
+alias ansible-playbook 'docker run -ti --rm \
+  -v ~/.ssh:/mnt/ssh \
+  -v (pwd):/apps \
+  -w /apps \
+  --network host \
+  alpine/ansible sh -c "mkdir -p /root/.ssh && cp -r /mnt/ssh/* /root/.ssh/ && chmod 700 /root/.ssh && chmod 600 /root/.ssh/* && exec ansible-playbook $argv"'

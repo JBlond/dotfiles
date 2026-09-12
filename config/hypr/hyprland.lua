ffb784006b419d3d9cddc345f7d052eee3c60d35
JBlond <leet31337@web.de>
2026-09-12 18:44:43 +0200

    Add lock

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -16,6 +16,8 @@ hl.env("QT_QPA_PLATFORMTHEME", "kde")
 hl.on("hyprland.start", function ()
     hl.exec_cmd("waybar")
     hl.exec_cmd("hyprpaper -c ~/.config/hypr/hyprpaper.conf")
+    hl.exec_cmd("mako")
+    hl.exec_cmd("hypridle")
 end)
 
 ---- ENVIRONMENT VARIABLES ----
@@ -223,7 +225,9 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 hl.bind("ALT + TAB", hl.dsp.layout("cyclenext"))
 hl.bind(mainMod .. " + TAB", hl.dsp.layout("cyclenext"))
 hl.bind("ALT + SHIFT + TAB", hl.dsp.layout("cycleprev"))
-
+hl.bind(mainMod .. " + L", function()
+    hl.exec_cmd("hyprlock")
+end)
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())

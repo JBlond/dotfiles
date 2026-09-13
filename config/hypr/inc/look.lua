87258b7407d203341542b75c6ebc89e27e763390
JBlond <leet31337@web.de>
2026-09-13 18:33:28 +0200

    update

--- config/hypr/inc/look.lua
+++ config/hypr/inc/look.lua
@@ -12,7 +12,7 @@ hl.config({
 
         col = {
             active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-          inactive_border = "rgba(595959aa)",
+            inactive_border = "rgba(595959aa)",
         },
 
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
@@ -34,16 +34,16 @@ hl.config({
 
         shadow = {
             enabled      = true,
-          range        = 4,
-          render_power = 3,
-          color        = 0xee1a1a1a,
+            range        = 4,
+            render_power = 3,
+            color        = 0xee1a1a1a,
         },
 
         blur = {
             enabled   = true,
-          size      = 3,
-          passes    = 1,
-          vibrancy  = 0.1696,
+            size      = 3,
+            passes    = 1,
+            vibrancy  = 0.1696,
         },
     },
 

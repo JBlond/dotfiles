d359a8934033ffedf1f43369efd04d3e8e76b809
JBlond <leet31337@web.de>
2026-09-13 18:42:54 +0200

    No cursor jump

--- config/hypr/inc/look.lua
+++ config/hypr/inc/look.lua
@@ -52,6 +52,14 @@ hl.config({
     },
 })
 
+hl.config({
+    cursor = {
+        no_warps = true
+    },
+    input = {
+        follow_mouse = 2
+    }
+})
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
 hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })

a5dccbe8bb6b69dd0b434b7eb4d07fe9e5a0aef2
JBlond <leet31337@web.de>
2026-09-25 12:01:17 +0200

    update

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -28,9 +28,9 @@ abbr dus 'du -hs * | sort -h'
 abbr dush 'du . -sh'
 #abbr greedy 'du -hs * | sort -rh'
 if command -q fd
-    abbr greedy 'fd --hidden --no-ignore --max-depth 1 --print0 '^' | xargs -0 -r du -hs | sort -rh'
+    abbr greedy 'fd --hidden --no-ignore --max-depth 1 --print0 '^' | xargs -0 -n1 -P4 du -sh | sort -rh'
 else
-    abbr greedy 'find . -mindepth 1 -maxdepth 1 -print0 | xargs -0 du -hs | sort -rh'
+    abbr greedy 'find . -mindepth 1 -maxdepth 1 -print0 | xargs -0 -n1 -P4 du -sh | sort -rh'
 end
 
 abbr vf 'cd'
